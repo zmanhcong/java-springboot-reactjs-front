6ee68f059643c989f2f26433fa3608bd06822a44
nguyenmanhcong <cng.buff@gmail.com>
2023-02-25 14:10:19 +0900

    add navbar in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './layout/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
     </div>
   );
 }
