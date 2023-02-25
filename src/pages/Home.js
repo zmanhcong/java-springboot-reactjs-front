152440aabaa6a608d343b22f7956a5d79f9c98f5
nguyenmanhcong <cng.buff@gmail.com>
2023-02-25 17:30:44 +0900

    add button: View, Edit, Delete USER

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,29 +10,40 @@ function Home() {
     },[])  //use ",[]" for run one time only
 
     const loadUsers = async() => {
-        const result = await axios.get("http://localhost:8080/users-list");
-        console.log(result.data);
+        const result = await axios.get("http://localhost:8080/users/users-list");
+        // console.log(result.data);
+        setUsers(result.data);
     }
 
     return ( 
         <div className='container'>
             <div className='py-4'>
                 <table className="table border shadow">
-                    <thead>
+                    <thead style={{ textAlign: 'left'}}>
                         <tr>
                         <th scope="col">#</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">User Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Action</th>
                         </tr>
                     </thead>
-                    <tbody>
-                        <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        </tr>
+                    <tbody style={{ textAlign: 'left'}}>
+                        {
+                            users.map((user, index)=> (
+                                <tr key={index}>
+                                    <th scope="row">{index+1}</th>
+                                    <td >{user.name}</td>
+                                    <td >{user.username}</td>
+                                    <td >{user.email}</td>
+                                    <td>
+                                        <button className='btn btn-primary mx-2'>View</button>
+                                        <button className='btn btn-outline-primary mx-2'>Edit</button>
+                                        <button className='btn btn-danger mx-2'>Delet</button>
+                                    </td>
+                                </tr>
+                            ))
+                        }
                     </tbody>
                 </table>
             </div>
