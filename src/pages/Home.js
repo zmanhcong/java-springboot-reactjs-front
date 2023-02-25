4078ed51d0c4debf7eae9383d7277b21e87898e9
nguyenmanhcong <cng.buff@gmail.com>
2023-02-25 14:27:45 +0900

    add axios for work with backend like: fetch data.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import axios from 'axios';
 
 function Home() {
     return ( 
         <div className='container'>
             <div className='py-4'>
-                <table className="table">
+                <table className="table border shadow">
                     <thead>
                         <tr>
                         <th scope="col">#</th>
