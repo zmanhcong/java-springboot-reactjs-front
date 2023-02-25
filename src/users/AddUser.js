ffa5f3eb11007317781957a09039c480b455ff8b
nguyenmanhcong <cng.buff@gmail.com>
2023-02-25 18:06:47 +0900

    create "Add user" form. HTML CSS

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function AddUser(){
+   
     return(
-        <div>
-            AddUser form
+        <div className="container">
+            <form>
+                <div className="form-row mt-3">
+                <div className="form-group " style={{ textAlign: 'left'}}>
+                    <label htmlFor="inputName" className="text-left" style={{ textAlign: 'left'}}>Name</label>
+                    <input 
+                        type="text" 
+                        className="form-control"
+                        id="inputName" 
+                        placeholder="Enter your name" 
+                    />
+                </div>
+                <div className="form-group mt-3" style={{ textAlign: 'left'}}>
+                    <label htmlFor="inputUsername">Username</label>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        id="inputUsername" 
+                        placeholder="Enter your username" 
+                    />
+                </div>
+                </div>
+                <div className="form-group mt-3" style={{ textAlign: 'left'}}>
+                    <label htmlFor="Email">Email</label>
+                    <input 
+                        type="email" 
+                        className="form-control" 
+                        id="inputEmail" 
+                        placeholder="Enter your email" 
+                    />
+                </div>
+                <button type="submit" className="btn btn-primary mt-3">Submit</button>
+                <button type="submit" className="btn btn-danger mt-3 mx-3">Cancle</button>
+            </form>
         </div>
+
     )
 }
