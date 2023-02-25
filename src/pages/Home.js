b4caed020171bf2753986d76b2fd7bff714fafd2
nguyenmanhcong <cng.buff@gmail.com>
2023-02-25 14:21:47 +0900

    Create homepage structure, then put bootstrap table

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+    return ( 
+        <div className='container'>
+            <div className='py-4'>
+                
+            </div>
+        </div>
+     );
+}
+
+export default Home;
