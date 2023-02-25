be2e27c13b19387a20e67bc9521a48a56e82d0b6
nguyenmanhcong <cng.buff@gmail.com>
2023-02-25 14:24:11 +0900

    add table from bootstrap to HomePage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,24 @@ function Home() {
     return ( 
         <div className='container'>
             <div className='py-4'>
-                
+                <table className="table">
+                    <thead>
+                        <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
      );
