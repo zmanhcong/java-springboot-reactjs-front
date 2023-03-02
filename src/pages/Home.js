73aabd1391c0432e6ae8838e2d41a266d676854d
nguyenmanhcong <cng.buff@gmail.com>
2023-03-03 00:34:42 +0900

    Delete user

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import { Button } from 'bootstrap';
 
 function Home() {
 
     const [users, setUsers] = useState([])
+    const {id} = useParams()
 
     useEffect(()=> {
         loadUsers();
@@ -14,6 +16,12 @@ function Home() {
         const result = await axios.get("http://localhost:8080/users/users-list");
         // console.log(result.data);
         setUsers(result.data);
+    }
+
+    // delete-user
+    const deleteUser = async (id)=> {
+        await axios.delete(`http://localhost:8080/users/delete/${id}`)
+        loadUsers()
     }
 
     return ( 
@@ -40,9 +48,14 @@ function Home() {
                                     <td >{user.username}</td>
                                     <td >{user.email}</td>
                                     <td>
-                                        <Link className="btn btn-primary mx-1" style={{padding:'3px'}}>View</Link>
+                                        <Link className="btn btn-primary mx-1" style={{padding:'3px'}} to={`/viewuser/{user.id}`}>View</Link>
                                         <Link className="btn btn-outline-primary mx-1" style={{padding:'3px'}} to={`/edituser/{user.id}`}>Edit</Link>
-                                        <Link className="btn btn-danger mx-1" style={{padding:'3px'}}>Delete</Link>  
+                                        <button 
+                                            className="btn btn-danger mx-1" style={{padding:'3px'}}
+                                            onClick={()=> deleteUser(user.id)}
+                                        >
+                                            Delete
+                                        </button>
                                     </td>
                                 </tr>
                             ))
